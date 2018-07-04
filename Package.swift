cbd2f428966ff9a4e415ed63577958d8d9ca89d0
Yuntian <da03@users.noreply.github.com>
2018-07-03 20:56:15 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "ZeroMQ",
+    products: [
+        .library(name: "ZeroMQ", targets: ["ZeroMQ"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/Zewo/CZeroMQ.git", majorVersion: 1, minor: 0),
+        .package(url: "https://github.com/da03/CZeroMQ.git", .branch("master")),
     ]
+    targets: [
+        .target(
+            name: "ZeroMQ",
+            dependencies: []),
+]
 )
